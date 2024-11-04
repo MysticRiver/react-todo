0a30afb013dba3a77cd0ff495c09f0290534685d
Tarik Carey <renewed@gmail.com>
2024-11-04 20:32:30 +0000

    Week 1 assignment

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,3 +1,23 @@
-import ".index.css"
+import "./App.css"
+let todoList = [];
+ todoList = [
+  { title: 'Complete Assignment', completed: false, id: 1 },
+  { title: 'Push to Github', completed: false,  id: 2 },
+  { title: 'Schedule Mentor Session', completed: false,  id: 3 },
+];
 
+export default function App() {
+  const listItems = todoList.map(todo =>
+    <li
+      key={todo.id}
+     
+    >
+      {todo.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
 
