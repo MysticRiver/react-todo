2c8423a4df84b295eaea596bed75e510a2a9768e
Tarik Carey <renewed@gmail.com>
2024-11-14 20:46:52 +0000

    Week 2 assignment- To Do Form Component completed

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 export default function App() {
   return (
     <div>
       <h1>My Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
