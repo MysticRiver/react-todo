4aef69aa2a556771a6a8411f6359018ef0fe4b90
Tarik Carey <renewed@gmail.com>
2024-12-05 17:37:16 +0000

    Week 6 assignment- Part 1 completed - defined a useEffect React Hook

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,17 +1,24 @@
 import "./App.css";
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; // Add useEffect import
 
 export default function App() {
   const [newTodo, setNewTodo] = useState('');
+  const [todoList, setTodoList] = useState([]); // Add todoList state
+
+  useEffect(() => {
+    // This effect will run whenever todoList changes
+    console.log('TodoList has been updated:', todoList);
+    // Add any logic you want to execute when todoList changes
+  }, [todoList]); // todoList as dependency
 
   return (
     <div>
       <h1>My Todo List</h1>
-      <AddTodoForm onAddTodo={setNewTodo} />   {/* Pass setNewTodo as onAddTodo prop */}
+      <AddTodoForm onAddTodo={setNewTodo} />
       <p>New Todo: {newTodo}</p>
       <TodoList />
     </div>
   );
-}
+
