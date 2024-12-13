0fdf456936c20d31f0a7bc56f6f12504abdacf29
Tarik Carey <renewed@gmail.com>
2024-12-13 20:45:50 +0000

    CRefactor Input with Label to use Component Composition & Add Auto-Focus to Input

--- ToDoApp/src/InputWithLabel.jsx
+++ ToDoApp/src/InputWithLabel.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const InputWithLabel = (props) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   return (
     <>
-    <label htmlFor="todoTitle">{props.label}</label>
+      <label htmlFor="todoTitle">Title:</label>
       <input 
+        ref={inputRef}
         type="text" 
         id="todoTitle"
         name="title"
         value={props.todoTitle}
         onChange={props.handleTitleChange}
-      />
+     />
     </>
   );
 };
