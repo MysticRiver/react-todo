43c5063323eb1fd98fdbd8fbe76d8f042d395ed7
Tarik Carey <renewed@gmail.com>
2025-01-13 20:32:57 -0500

    Completed Assignment
    Data Fetching Data Refetching Memoized Handlers
    Week 10 of 17
    
    Jan 01, 2025 - Jan 07, 2025

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -7,19 +7,44 @@ export default function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   
-  useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({ 
-          data: {
-            todoList: JSON.parse(localStorage.getItem('savedTodoList')) || []
-          } 
-        });
-      }, 2000);
-    }).then((result) => {
-      setTodoList(result.data.todoList);
+  const fetchData = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_TOKEN}`
+      }
+    };
+
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  
+
+    try {
+      const response = await fetch(url, options);
+  
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+      } 
+
+      const data = await response.json();
+      
+      const todosFromAPI = await response.json();
+      
+      const todos = todosFromAPI.records.map((todo) => ({
+        id: todo.id,
+        title: todo.fields.title
+      }));
+
+      setTodoList(todos);
       setIsLoading(false);
-    });
+
+    } catch (error) {
+      console.log(error.message);
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
   }, []);
 
   useEffect(() => {
