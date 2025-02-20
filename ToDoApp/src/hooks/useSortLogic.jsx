08e6d464c420e5f346cbf0206bb1c9a7a3c7e673
Tarik Carey <renewed@gmail.com>
2025-02-20 17:13:02 +0000

    Updated the useSortLogic.jsx file with improved sort logic

--- ToDoApp/src/hooks/useSortLogic.jsx
+++ ToDoApp/src/hooks/useSortLogic.jsx
@@ -1,23 +1,11 @@
 import { useState } from 'react';
 
-function useSortLogic(setTodoList) {
+function useSortLogic(fetchData) {
   const [sortOrder, setSortOrder] = useState('asc');
 
   const toggleSortOrder = () => {
     setSortOrder(prevOrder => prevOrder === 'asc' ? 'desc' : 'asc');
-    setTodoList(prevList => [...prevList].sort((a, b) => {
-      if (sortOrder === 'asc') {
-        // Sort descending
-        if (a.title > b.title) return -1;
-        if (a.title < b.title) return 1;
-        return 0;
-      } else {
-        // Sort ascending
-        if (a.title < b.title) return -1;
-        if (a.title > b.title) return 1;
-        return 0;
-      }
-    }));
+    fetchData(sortOrder === 'asc' ? 'desc' : 'asc');
   };
 
   return {
